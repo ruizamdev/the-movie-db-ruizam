eeb8a288974238941660617a9b90500b0696cf3a
Armando Ruiz <artmx@proton.me>
2025-03-20 08:29:55 -0600

    IN PROGRESS: movie-details-window
    Agregué una ventana con position fixed para que no tenga scroll, solo se ve la ventana y  la barra de navegación, y esta el layout de la ventana solo falta maquetar el interior, afinar detalles de estilo y agregar animaciones

--- src/main.js
+++ src/main.js
@@ -34,22 +34,39 @@ function searchBarInteractive () {
 }
 
 async function getTrendingMoviesPreview () {
-  const response = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=bbda25d057b9c8c6bbf7f6966d3f9f1b`);
+  const response = await fetch(`https://api.themoviedb.org/3/trending/movie/day?api_key=bbda25d057b9c8c6bbf7f6966d3f9f1b&language=es-MX`);
   const data = await response.json();
   
   const movies = data.results;
   const moviesSorted = movies.sort((a, b) => b.popularity - a.popularity);
   // console.log({ movies });
 
+  // function to set the color of the rate and popularity values
+  function setValuesColor (value) {
+    if (value >= 7 || value <= 10) {
+      return 'color: #00bfa5';
+    } else if (movieRateValue >= 4.0 || movieRateValue <= 6.9) {
+      return 'color: #f9a825';
+    } else if (movieRateValue >= 0 || movieRateValue <= 3.9) {
+      return 'color: #e53935';
+    } else {
+      return 'color: #77767c';
+    }
+  }
+
   // hero populating
+  // debugger;
   const hero = document.querySelector('#hero');
   hero.style.backgroundImage = `url(https://image.tmdb.org/t/p/original${movies[0].backdrop_path})`
   const heroTitle = hero.querySelector('.hero-info__title');
-  heroTitle.innerHTML = `${moviesSorted[0].original_title}`
+  heroTitle.innerHTML = `${moviesSorted[0].original_title}`;
   const heroYear = hero.querySelector('.hero-info__year');
   heroYear.innerHTML = `${moviesSorted[0].release_date.substr(0, 4)}`;
+  heroYear.setAttribute('style', 'font-weight: 700;');
   const heroRate = hero.querySelector('.hero-info__rate');
-  heroRate.innerHTML = `${moviesSorted[0].vote_average}`;
+  const heroRateValue = moviesSorted[0].vote_average.toFixed(1);
+  heroRate.innerHTML = `${heroRateValue}`;
+  heroRate.setAttribute('style', 'font-weight: 700;');
   const heroOverview = hero.querySelector('.hero-info__synopsis');
   heroOverview.innerHTML = moviesSorted[0].overview;
 
@@ -74,25 +91,28 @@ async function getTrendingMoviesPreview () {
     const topNumber = document.createElement('span');
     topNumber.classList.add('top-number');
     topNumber.innerHTML = `#${counter}`;
+    topNumber.style.fontWeight = '700';
     const movieTitle = document.createElement('span');
     movieTitle.classList.add('movie-title');
     movieTitle.innerHTML = movie.title;
     const movieYear = document.createElement('span');
     movieYear.classList.add('movie-year');
-    movieYear.innerHTML = `Release year: ${movie.release_date.substr(0, 4)}`
+    movieYear.innerHTML = `Año de estreno: ${movie.release_date.substr(0, 4)}`
     const movieRate = document.createElement('span');
+    const movieRateValue = movie.vote_average.toFixed(1);
     movieRate.classList.add('movie-rate');
-    movieRate.innerHTML = `Rate: ${movie.vote_average.toFixed(1)}`;
+    movieRate.innerHTML = `Calificación: <span class="movie-rate__value" style="${setValuesColor(movieRateValue)}">${movieRateValue}</span>`;
     const movieVotes = document.createElement('span');
     movieVotes.classList.add('movie-votes');
-    movieVotes.innerHTML = `Votes: ${movie.vote_count}`;
+    movieVotes.innerHTML = `Votos: ${movie.vote_count}`;
     const moviePopularity = document.createElement('span');
     moviePopularity.classList.add('movie-popularity');
-    moviePopularity.innerHTML = `Popularity: ${movie.popularity.toFixed(1)}`;
+    moviePopularity.innerHTML = `Popularidad: <span class="movie-popularity__value">${movie.popularity.toFixed(1)}`;
     const moreDetailsBtn = document.createElement('button');
     moreDetailsBtn.classList.add('moreDetails-btn');
     moreDetailsBtn.innerHTML = "More details"
 
+    movieDetailsContainer.appendChild(topNumber);
     movieDetailsContainer.appendChild(movieTitle);
     movieDetailsContainer.appendChild(movieYear);
     movieDetailsContainer.appendChild(movieRate);
@@ -115,8 +135,6 @@ async function getTrendingMoviesPreview () {
 
 
 }
-
-
 
 navbarScrollTransform();
 searchBarInteractive();
