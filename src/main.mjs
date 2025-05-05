da2ac7577f2e799ec07b67079654a18ad0bffef2
Armando Ruiz <artmx@proton.me>
2025-05-04 18:00:48 -0600

    Refactors component structure and removes unused code
    Restructures components for better organization and maintainability.
    
    Moves components into dedicated directories within the src directory and renames 'header-navbar' to 'headerNavbar' for consistency.
    
    Removes unused files and components, including main.mjs, companies.mjs, movie-info-card, trendingSection and searchBarInteractive, simplifying the codebase.

--- src/main.mjs
+++ src/main.mjs
@@ -1,40 +1,5 @@
-// debugger;
-/* Import components */
-import { TheHero } from "./components/hero/hero.mjs";
-import { HeaderNavbar } from "./components/header-navbar/header-navbar.mjs";
-import { MovieInfoCard } from "./components/movie-info-card/movieInfoCard.mjs";
-import { getTrendingMovies } from "./goHome.mjs";
+import { TheHero } from './components/the-hero/theHero.mjs';
+import { HeaderNavbar } from './components/header-navbar/headerNavbar.mjs';
 
-/* Define components */
 customElements.define('the-hero', TheHero);
 customElements.define('header-navbar', HeaderNavbar);
-customElements.define('movie-info-card', MovieInfoCard);
-
-
-/* function delay(ms) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-};
-
-async function loading() {
-  movieDetailsWindow.classList.remove('inactive');
-  movieDetailsWindowLoader.classList.remove('inactive');
-  await delay(1500);
-  movieDetailsWindow.classList.add('inactive');
-  movieDetailsWindowLoader.classList.add('inactive');
-};
-
-loading(); */
-
-if(location.hash.startsWith('#peliculas')) {
-
-} else if (location.hash.startsWith('#series')) {
-
-} else if (location.hash.startsWith('#personas')) {
-
-} else if (location.hash.startsWith('#Buscar')) {
-
-} else if (location.hash.startsWith('#favoritos')){
-
-} else {
-  getTrendingMovies();
-}
